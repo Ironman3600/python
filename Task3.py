3a9473e6cdfe5d135806bc08a3ce612443d5e064
uday kumar <157950579+Ironman3600@users.noreply.github.com>
2025-08-16 14:38:17 +0530

    Add files via upload
    About Python course of Control Structures

---
+++ Task3.py
@@ -0,0 +1,3 @@
+for i in range(51):
+    sum = i * (i + 1) // 2
+    print("The sum of numbers from 1 to 50 is ",sum)
---
+++ Task4.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+if a % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+s = n(n+1)/2
+
+
